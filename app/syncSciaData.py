7d02ee3fa2c7952a0c4ed2f233f9ea0c466c1135
ZhoU Rokeman <xzrokeman@163.com>
2023-12-26 07:36:07 +0800

    Update syncSciaData.py

--- app/syncSciaData.py
+++ app/syncSciaData.py
@@ -2,26 +2,29 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 from initDB import mySqlEngine, sqliteEngine
+from sqlalchemy.exc import OperationalError
 
 
-# Case fee info from old website
+# Case fee info from SCIA old website
 def sync_case_fee_data():
     remote_frame = pd.read_sql(
         "select * from v_bank_records", mySqlEngine, dtype={"u_t": str}
     )
+    try:
+        # 从本地SQLite数据库获取原始数据
+        local_frame = pd.read_sql("select * from v_bank_records", sqliteEngine)
+        local_frame["u_t"] = local_frame["u_t"].astype("string")
+        # 将远程数据和本地数据进行合并（去重）
+        merged_frame = pd.concat([local_frame, remote_frame]).drop_duplicates(
+            subset=["u_t", "txnamt", "ibkname", "acntname", "bill_code"], keep="last"
+        )
 
-    # 从本地SQLite数据库获取原始数据
-    local_frame = pd.read_sql("select * from v_bank_records", sqliteEngine)
-    local_frame["u_t"] = local_frame["u_t"].astype("string")
-    # 将远程数据和本地数据进行合并（去重）
-    merged_frame = pd.concat([local_frame, remote_frame]).drop_duplicates(
-        subset=["u_t", "txnamt", "ibkname", "acntname", "bill_code"], keep="last"
-    )
-
-    # 将合并后的数据更新到本地SQLite数据库
-    with sqliteEngine.connect() as sconn:
-        merged_frame.to_sql("v_bank_records", sconn, index=False, if_exists="replace")
-
+        # 将合并后的数据更新到本地SQLite数据库
+        with sqliteEngine.connect() as sconn:
+            merged_frame.to_sql("v_bank_records", sconn, index=False, if_exists="replace")
+    except OperationalError:
+        with sqliteEngine.connect() as sconn:
+            remote_frame.to_sql("v_bank_records", sconn, index=False, if_exists="replace")
 
 # sync Kingdee EAS base data
 if __name__ == "__main__":
